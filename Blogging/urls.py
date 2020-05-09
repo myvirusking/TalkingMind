a96d3a6363398c3a20cec83bf6db9b5acfe8ea4b
UnknowN-ag <ag528927@gmail.com>
2020-05-09 23:22:14 +0530

    single page

--- Blogging/urls.py
+++ Blogging/urls.py
@@ -73,6 +73,10 @@ urlpatterns = [
 
     path('user/saved_post/save/', blog_views.save_post, name="save-post2"),
 
+    path('post/<int:pid>/', blog_views.single_post, name="single-post"),
+
+    path('post/<int:pid>/save/', blog_views.save_post, name="post-detail-save")
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/forms.py
+++ blog/forms.py
@@ -7,16 +7,16 @@ from users.models import ArticleCategory
 class NewPostForm(forms.ModelForm):
     title = forms.CharField(widget=TextInput(attrs={'class': 'form-control','placeholder':'Enter title','style':'margin-top:2px;'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': '8','placeholder':'Enter content'}))
-    category = forms.ChoiceField(choices=[(category.id, category.name) for category in ArticleCategory.objects.all()],widget=forms.Select(
-        attrs={'class': 'form-control','style':'margin-top:2px;'}
-    ))
+    # category = forms.ChoiceField(choices=[(category.id, category.name) for category in ArticleCategory.objects.all()],widget=forms.Select(
+    #     attrs={'class': 'form-control','style':'margin-top:2px;'}
+    # ))
     image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control','style':'margin-top:2px;'}),required=False)
 
     class Meta:
         model = Post
         fields = ['title', 'content','category','image']
 
-    def clean_category(self):
-        category_id = self.cleaned_data['category']
-        category_obj = ArticleCategory.objects.get(id = category_id)
-        return category_obj
+    # def clean_category(self):
+    #     category_id = self.cleaned_data['category']
+    #     category_obj = ArticleCategory.objects.get(id = category_id)
+    #     return category_obj
