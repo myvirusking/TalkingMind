4373b5b8011665b7f49394c0eaa3d3cb06a8e462
Prashant <pkj.011999@gmail.com>
2020-05-29 15:48:05 +0530

    Change password implementation is done. Since the proper UI has not been implemented yet so go to /change-password to use the feature

--- Blogging/settings.py
+++ Blogging/settings.py
@@ -42,10 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.templatetags.custom_tag',
-    'django_otp',
-    'django_otp.plugins.otp_static',
-    'django_otp.plugins.otp_totp',
-    'two_factor',
+
 
 ]
 
@@ -56,7 +53,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django_otp.middleware.OTPMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
